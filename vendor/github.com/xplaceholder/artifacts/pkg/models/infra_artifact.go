d7b77247e60c2fdb37ffcceab4e233a6d4a06dec
Andy Liu <andyliuliming@outlook.com>
2018-10-28 14:38:33 +0800

    sample ginkgo.

---
+++ vendor/github.com/xplaceholder/artifacts/pkg/models/infra_artifact.go
@@ -0,0 +1,5 @@
+package models
+
+type InfraArtifact struct {
+	BaseArtifact
+}
---
+++ vendor/github.com/xplaceholder/ashandler/generator/as_generator.go
@@ -0,0 +1,10 @@
+package generator
+
+import "github.com/xplaceholder/artifacts/pkg/models"
+
+type ASGenerator struct {
+}
+
+func (a *ASGenerator) Generate(d *models.DeploymentArtifact) error {
+	return nil
+}
