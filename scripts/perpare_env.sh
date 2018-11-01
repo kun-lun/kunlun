7fb109438e8292a9176ba850b2fbd5186a3b661d
andyliuliming <andyliuliming@outlook.com>
2018-11-01 15:37:38 +0800

    update the prepare environment script.

--- scripts/perpare_env.sh
+++ scripts/perpare_env.sh
@@ -5,15 +5,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 projects_array=("artifacts" "common" "deployment-producer" "executor" \
 "migration-producer" "test-infra" "verification-producer" \
 "ashandler" "digester" "infra-producer" "report-producer" \
-"tfhandler" "xplaceholder")
+"tfhandler" "kunlun")
 
 for i in "${projects_array[@]}"
 do
-  go get github.com/xplaceholder/$i
-  pushd $GOPATH/src/github.com/xplaceholder/$i
+  go get github.com/kun-lun/$i
+  pushd $GOPATH/src/github.com/kun-lun/$i
     echo "now checking out ${i}"
     git remote remove origin
-    git remote add origin git@github.com:xplaceholder/$i.git
+    git remote add origin git@github.com:kun-lun/$i.git
     # checkout the draft branch for now, remove this after v0.1 merged to the master.
     git fetch
     git checkout draft
