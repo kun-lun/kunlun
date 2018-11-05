fe86237081f70cc04f1bde1aaf05c6633f3b5973
andyliuliming <andyliuliming@outlook.com>
2018-11-05 11:21:15 +0800

    fix the shortname for the command.

--- config/global_flags.go
+++ config/global_flags.go
@@ -2,10 +2,10 @@ package config
 
 type GlobalFlags struct {
 	Help      bool   `short:"h" long:"help"`
-	Debug     bool   `short:"d" long:"debug"        env:"KID_DEBUG"`
+	Debug     bool   `short:"d" long:"debug"        env:"KL_DEBUG"`
 	Version   bool   `short:"v" long:"version"`
 	NoConfirm bool   `short:"n" long:"no-confirm"`
-	StateDir  string `short:"s" long:"state-dir"    env:"KID_STATE_DIRECTORY"`
+	StateDir  string `short:"s" long:"state-dir"    env:"KL_STATE_DIRECTORY"`
 	EnvID     string `          long:"name"`
-	IAAS      string `          long:"iaas"         env:"KID_IAAS"`
+	IAAS      string `          long:"iaas"         env:"KL_IAAS"`
 }
